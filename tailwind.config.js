c3f638f1ea47ef765671c98942620e50087cfabe
Kent Delante <kent@kentdelante.com>
2024-11-12 17:52:10 +0800

    Make several adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-	"./index.html"
-  ],
-  theme: {
-    extend: {
-	  animation: {
-		"spin-slow": "spin 5s linear infinite",
-		"spin-slow-reverse": "spin 5s linear infinite reverse"
-	  }
+	content: [
+		"./index.html"
+	],
+	theme: {
+		fontFamily: {
+			"josefin": ["Josefin Sans", "Helvetica", "sans-serif"]
+		},
+		extend: {
+			animation: {
+				"spin-slow": "spin 5s linear infinite",
+				"spin-slow-reverse": "spin 5s linear infinite reverse"
+			}
+		},
 	},
-  },
-  plugins: [],
+	plugins: [],
 }
 
